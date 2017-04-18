50bdb137b60a89722d1592b2c66bf56335a95c68
Alex <alex.catala.cabello@gmail.com>
2017-04-18 19:45:21 +0200

    latest zooming changes

--- renderer.js
+++ renderer.js
@@ -15,10 +15,11 @@ ipcRender.on('selected-file', function(event, path) {
 
 function drawChart(path) {
     var fs = require('fs');
+    var yInit, yPercentInit, zoomableInit;
 
     var margin = {
             top: 20,
-            right: 20,
+            right: 50,
             bottom: 30,
             left: 50
         },
@@ -43,14 +44,53 @@ function drawChart(path) {
         .yScale(y);
 
     var xAxis = d3.axisBottom(x);
-
+    var xTopAxis = d3.axisTop(x);
     var yAxis = d3.axisLeft(y);
+    var yRightAxis = d3.axisRight(y);
+
+    var ohlcAnnotation = techan.plot.axisannotation()
+        .axis(yAxis)
+        .orient('left')
+        .format(d3.format(',.2f'));
+
+    var ohlcRightAnnotation = techan.plot.axisannotation()
+        .axis(yRightAxis)
+        .orient('right')
+        .translate([width, 0]);
+
+    var timeAnnotation = techan.plot.axisannotation()
+        .axis(xAxis)
+        .orient('bottom')
+        .format(d3.timeFormat('%Y-%m-%d'))
+        .width(65)
+        .translate([0, height]);
+
+    var timeTopAnnotation = techan.plot.axisannotation()
+        .axis(xTopAxis)
+        .orient('top');
+
+    var crosshair = techan.plot.crosshair()
+        .xScale(x)
+        .yScale(y)
+        .xAnnotation([timeAnnotation, timeTopAnnotation])
+        .yAnnotation([ohlcAnnotation, ohlcRightAnnotation])
+        .on("enter", enter)
+        .on("out", out)
+        .on("move", move);
+
+
 
     var svg = d3.select("#chart").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var coordsText = svg.append('text')
+        .style("text-anchor", "end")
+        .attr("class", "coords")
+        .attr("x", width - 5)
+        .attr("y", 15);
 
     svg.append("clipPath")
         .attr("id", "clip")
@@ -60,31 +100,29 @@ function drawChart(path) {
         .attr("width", width)
         .attr("height", y(0) - y(1));
 
-        svg.append("g")
-            .attr("class", "ohlc")
-            .attr("clip-path", "url(#clip)");
+    svg.append("g")
+        .attr("class", "ohlc")
+        .attr("clip-path", "url(#clip)");
 
 
-        svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")");
+    /*svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")");
 
-        svg.append("g")
-                .attr("class", "y axis")
-            .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Price ($)");
+    svg.append("g")
+        .attr("class", "y axis")
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Price ($)");*/
 
-        svg.append("rect")
-                .attr("class", "pane")
-                .attr("width", width)
-                .attr("height", height)
-                .call(zoom);
-
-
+    svg.append("rect")
+        .attr("class", "pane")
+        .attr("width", width)
+        .attr("height", height)
+        .call(zoom);
 
     fs.readFile(path, 'utf8', function(err, dataString) {
         var accessor = ohlc.accessor();
@@ -99,27 +137,74 @@ function drawChart(path) {
                 close: +d.Close,
                 volume: +d.Volume
             };
-        }).sort(function(a, b) { return d3.ascending(accessor.d(a), accessor.d(b)); });
+        }).sort(function(a, b) {
+            return d3.ascending(accessor.d(a), accessor.d(b));
+        });
 
         x.domain(data.map(accessor.d));
         y.domain(techan.scale.plot.ohlc(data, accessor).domain());
 
         svg.select("g.ohlc").datum(data);
-        console.log(svg.select("g.ohlc").datum());
+        //console.log(svg.select("g.ohlc").datum());
+        svg.append("g")
+            .attr("class", "x axis")
+            //.attr("transform", "translate(0," + height + ")")
+            .call(xTopAxis);
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            //.attr("transform", "translate(" + width + ",0)")
+            .call(yAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + width + ",0)")
+            .call(yRightAxis);
+
+        /*svg.append('g')
+            .attr("class", "crosshair")
+            .datum({
+                x: x.domain()[80],
+                y: 67.5
+            })
+            .call(crosshair)
+            .each(function(d) {
+                move(d);
+            }); // Display the current data
+*/
+
+// Stash for zooming
+        zoomableInit = x.zoomable().domain([0, data.length]).copy(); // Zoom in a little to hide indicator preroll
+        yInit = y.copy();
         draw();
 
         // Associate the zoom with the scale after a domain has been applied
         // Stash initial settings to store as baseline for zooming
-        zoomableInit = x.zoomable().clamp(false).copy();
+        //zoomableInit = x.zoomable().clamp(false).copy();
     });
 
     function zoomed() {
-        var rescaledY = d3.event.transform.rescaleY(y);
+        /*var rescaledY = d3.event.transform.rescaleY(y);
+        //var rescaledYRigth = d3.event.transform.rescaleY(y);
+
         yAxis.scale(rescaledY);
         ohlc.yScale(rescaledY);
 
+        //yRightAxis.scale(rescaledYRigth);
+        //ohlc.yScale(rescaledYRigth);
+
         // Emulates D3 behaviour, required for financetime due to secondary zoomable scale
         x.zoomable().domain(d3.event.transform.rescaleX(zoomableInit).domain());
+
+        draw();
+*/
+        x.zoomable().domain(d3.event.transform.rescaleX(zoomableInit).domain());
+        y.domain(d3.event.transform.rescaleY(yInit).domain());
 
         draw();
     }
@@ -130,6 +215,25 @@ function drawChart(path) {
         // Use this if underlying data is not changing
         //svg.select("g.ohlc").call(ohlc.refresh);
         svg.select("g.x.axis").call(xAxis);
-        svg.select("g.y.axis").call(yAxis)
+        svg.select("g.y.axis").call(yAxis);
+        svg.select("g.x.axis").call(xTopAxis);
+        svg.select("g.y.axis").call(yRightAxis);
+        //svg.select("g.x.axis").call(xTopAxis);
+      //  svg.select("g.y.axis").call(yRightAxis);
+
+    }
+
+    function enter() {
+        coordsText.style("display", "inline");
+    }
+
+    function out() {
+        coordsText.style("display", "none");
+    }
+
+    function move(coords) {
+        coordsText.text(
+            timeAnnotation.format()(coords.x) + ", " + ohlcAnnotation.format()(coords.y)
+        );
     }
 }
